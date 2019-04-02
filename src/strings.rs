cef7a06230d67a699cac8588527877d17950574f
Romeo Disca <romeo.disca@gmail.com>
2019-04-02 21:24:07 +0200

    implement find char in string unit test

--- src/strings.rs
+++ src/strings.rs
@@ -18,3 +18,31 @@ pub fn prepare(s: &str) -> String {
     let re = Regex::new(r".").expect("Can't create regular expression");
     re.replace_all(s, "_").to_string()
 }
+
+pub fn find_char_in_string(ch: &str, s: &str) -> bool {
+
+    let (head, rest) = get_first_unicode_char(s);
+    if head.eq(ch) {
+        return true
+    }
+    else if rest.ne("") {
+        return find_char_in_string(ch, rest)
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_find_char_in_string() {
+        assert_eq!(false, find_char_in_string("", "hallo"));
+        assert_eq!(false, find_char_in_string("c", "hallo"));
+        assert_eq!(true, find_char_in_string("a", "hallo"));
+        assert_eq!(true, find_char_in_string("h", "hallo"));
+        assert_eq!(true, find_char_in_string("o", "hallo"));
+    }
+}
