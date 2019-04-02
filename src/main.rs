bf35c8f29c0d56bfeb82f7f13faddc7caeb6c7f3
Romeo Disca <romeo.disca@gmail.com>
2019-04-02 20:08:54 +0200

    enter a message

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,12 @@ use graphics::{*, Hangman::*};
 
 fn main() {
     let state = Splash;
-    &GRAPHICS.get(&state).unwrap().draw();
+    GRAPHICS.get(&state).unwrap().draw();
+
+    let mut line = String::new();
+    println!("Type something: ");
+    match std::io::stdin().read_line(&mut line) {
+        Ok(_) => println!("You typed: {}", &line),
+        _ => eprintln!("Can't process your entry"),
+    }
 }
